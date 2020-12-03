e907f8eed20c88607a792043b1d4cacd6d9c0433
Brandee Gleed <brandeegleed1@gmail.com>
2020-12-03 12:52:01 -0500

    added notes
    neet to fix if else into ternary

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -13,7 +13,7 @@ const seasonConfig = {
 };
 
 const getSeason = (lat, month) => {
-
+ //TODO change from if to ternary(s)
 	if (month > 2 && month < 9) {
 		return lat > 0 ? 'summer' : 'winter';
 	} else {
